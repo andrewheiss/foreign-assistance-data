3ca077d4ab7d7dcd13346bffd5263176b34b77aa
Andrew Heiss <andrew@andrewheiss.com>
2025-02-03 16:49:26 -0500

    Add link to Dropbox .zip

--- data-processing/build-db.sh
+++ data-processing/build-db.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+curl -L -o data-raw_2025-02-03.zip "https://www.dropbox.com/scl/fi/btzeaaq84wdpahb7c033g/data-raw_2025-02-03.zip?rlkey=atqpr633ayy4ya9p428vi6lon&dl=1"
 unzip data-raw_2025-02-03.zip -d data-raw_2025-02-03 
 uv run csvs-to-sqlite data-raw ../data/2025-02-03_foreign-assistance.db
